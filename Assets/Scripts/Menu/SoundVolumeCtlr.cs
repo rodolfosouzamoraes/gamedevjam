ee9081e7c229f0f39ccab045dc53a52a65b89e00
Fábio Lamin <fabio_lamin@hotmail.com>
2020-05-06 21:39:50 -0300

    Implementing Sound Volume Controller

---
+++ Assets/Scripts/Menu/SoundVolumeCtlr.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SoundVolumeCtlr : MonoBehaviour
+{
+    [SerializeField] private Scrollbar globalSound;
+    [SerializeField] private Scrollbar effectsSound;
+
+    private void Awake()
+    {
+        SetScrollbarValues();
+    }
+
+    private void SetScrollbarValues()
+    {
+        globalSound.value = PlayerPrefs.GetFloat("GlobalSound");
+        effectsSound.value = PlayerPrefs.GetFloat("EffectsSound");
+    }
+
+    public void SaveSound()
+    {
+        PlayerPrefs.SetFloat("GlobalSound", globalSound.value);
+        PlayerPrefs.SetFloat("EffectsSound", effectsSound.value);
+        AudioListener.volume = PlayerPrefs.GetFloat("GlobalSound");
+    }
+}
